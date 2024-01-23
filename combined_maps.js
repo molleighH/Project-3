f82ddc3705b16ac14dc9daf5d1574295d345b8db
Tasha Christensen <123216506+elasmo4@users.noreply.github.com>
2024-01-23 17:50:49 -0500

    Add files via upload

---
+++ combined_maps.js
@@ -0,0 +1,129 @@
+// The map
+let places = ['Oakland', 'Washtenaw', 'Wayne']
+
+// Creating the map object
+let myMap = L.map("map", {
+    center: [42.382980, -83.372582],
+    zoom: 10
+  });
+  
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
+  // URL to the Superfund GeoJSON (Michigan)
+  const superfundSites = "https://gispub.epa.gov/arcgis/rest/services/OEI/FRS_INTERESTS/MapServer/21/query?outFields=*&where=STATE_CODE%3D%27MI%27&f=geojson";
+  
+  // Getting Superfund GeoJSON data
+  d3.json(superfundSites).then(({features}) => {
+
+    console.log(features);
+
+    // Filtering for Oakland, Washtenaw, and Wayne counties
+    let filter = features.filter(obj => places.map(text => text.toUpperCase()).includes(obj.properties.COUNTY_NAME));
+    
+    // Add the filtered GeoJSON with information provided upon clicking on pins
+    L.geoJson(filter, {onEachFeature}).addTo(myMap);
+
+    // Information when clicking on pin
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>${feature.properties.PRIMARY_NAME}</h3> <hr> <p>${feature.properties.LOCATION_ADDRESS}</p> <p>${feature.properties.CITY_NAME}, MI ${feature.properties.POSTAL_CODE}</p>`)
+    };
+
+  });
+
+  // URL to the county GeoJSON data
+  const counties = "https://gisagocss.state.mi.us/arcgis/rest/services/OpenData/michigan_geographic_framework/MapServer/0/query?outFields=*&where=1%3D1&f=geojson"
+
+  // Retrieving Michigan county GeoJSON data
+  d3.json(counties).then(({features}) => {
+
+    // Filtering for Oakland, Washtenaw, and Wayne counties
+    let filter = features.filter(obj => places.includes(obj.properties.NAME));
+
+    // Hard code heat map for severity of asthma
+    L.geoJSON(filter, {
+      style: function (feature) {
+          return {fillColor: feature.properties.NAME == 'Wayne' ? 'red' : 'orange'}}
+  }).addTo(myMap);
+  });
+
+// Filtered AQI data
+let airData = "./data.csv";
+
+d3.csv(airData).then(function(data) {
+
+    // Grab latitude and longtiude for AQI montiors
+    for (let i = 0; i < data.length; i++) {
+
+        var myIcon = L.icon({
+            iconUrl: "./lung.png",
+            iconSize: [40, 60]})
+
+        // Add locations to map
+        L.marker([data[i].Latitude, data[i].Longitude], {icon: myIcon}).addTo(myMap);
+}});
+
+// Filtered asthma data
+let asthmaData = "./asthma_data.csv"
+
+d3.csv(asthmaData).then(({data}) => {
+
+    // Hard code county center
+    let centerOak = [42.592192, -83.336188]
+    let centerWash = [42.307649, -83.847302]
+    let centerWayne = [42.2814269, -83.3863214]
+
+    var myIcon = L.icon({
+        iconUrl: "./inhaler.png",
+        iconSize: [40, 60]})
+
+    // Add icon for county center to map
+    L.marker(centerOak, {icon: myIcon}).addTo(myMap),
+    L.marker(centerWash, {icon: myIcon}).addTo(myMap)
+    L.marker(centerWayne, {icon:myIcon}).addTo(myMap)});
+
+
+// // The asthma chart
+// import Chart from 'chart.js/auto'
+
+// // Render asthma chart
+// new Chart(
+//   document.getElementById('chartJs'),
+//   {
+//     type: 'line',
+//     data: {
+//       labels: ["2016", "2017", "2018", "2019"],
+//       datasets: [
+//         {
+//           label: "Oakland Asthma Hospitalization Rates",
+//           data: [5.3, 6.2, 6.0, 5.8]},
+//         {
+//           label: "Washtenaw Asthma Hospitalization Rates",
+//           data: [5.9, 5.7, 5.8, 5.1]},
+//         {
+//           label: "Wayne Asthma Hospitalization Rates",
+//           data: [12.8, 13.3, 12.9, 13.1]}
+//         ]},
+//     options: {
+//       responsive: true,
+//       scales: {
+//         x: {
+//           display: true,
+//           title: {
+//             display: true,
+//             text: "Year"
+//           }
+//         },
+//         y: {
+//           display:true,
+//           title: {
+//             display:true,
+//             text: "Age-Adjusted Rate of Hospitalization per 10,000 Population"
+//           }
+//         }
+//       }
+//     }
+//   });
+
